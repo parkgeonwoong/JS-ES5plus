0e5194fb6412162ecc98f99fce253a0609668a07
Woong <kweyz@daum.net>
2021-07-31 18:51:20 +0900

    Array

--- array.js
+++ array.js
@@ -148,20 +148,27 @@ console.clear()
   
   // Q9. compute students' average score
   {
-    const result = students.reduce((prev, curr) => {
-      console.log('-----------')
-      console.log(prev)
-      console.log(curr)
-      return curr
-    }, 0)
+    // reduce: 배열하나하나 돌면서 누적한다.
+    // prev: 전에 있던것 받는다.
+    const result = students.reduce((prev, curr) => prev + curr.score, 0)
+    console.log(result / students.length)
   }
   
   // Q10. make a string containing all the scores
   // result should be: '45, 80, 90, 66, 88'
   {
+    const result = students
+    .map(student => student.score)
+    .filter((score) => score >= 50)
+    .join()
+    console.log(result)
   }
   
   // Bonus! do Q10 sorted in ascending order
   // result should be: '45, 66, 80, 88, 90'
   {
+    const result = students.map(student => student.score)
+    .sort((a, b) => a - b)
+    .join()
+    console.log(result)
   }
