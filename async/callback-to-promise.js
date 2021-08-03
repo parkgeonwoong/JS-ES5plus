842afca64a505c952f638ac0876999444054a474
unknown <kweyz@daum.net>
2021-08-03 11:30:42 +0900

    Callback To Promise

--- async/callback-to-promise.js
+++ async/callback-to-promise.js
@@ -1,44 +1,41 @@
-// 콜백 지옥 예제
+// 콜백 지옥 예제 해결 
 class UserStorage {
-    loginUser(id, password, onSuccess, onError) {
-        setTimeout(() => {
-            if (
-                (id === 'woong' && password === 'dream') ||
-                (id === 'coder' && password === 'academy')
-            ) {
-                onSuccess(id);
-            } else {
-                onError(new Error('not found'))
-            }
-        }, 2000)
+    loginUser(id, password) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (
+                    (id === 'woong' && password === 'dream') ||
+                    (id === 'coder' && password === 'academy')
+                ) {
+                    resolve(id);
+                } else {
+                    reject(new Error('not found'))
+                }
+            }, 2000)
+        })
     }
 
-    getRoles(user, onSuccess, onError) {
-        setTimeout(() => {
-            if (user === 'woong') {
-                onSuccess({
-                    name: 'woong',
-                    role: 'admin'
-                })
-            } else {
-                onError(new Error('no access'))
-            }
-        }, 1000)
+    getRoles(user) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (user === 'woong') {
+                    resolve({
+                        name: 'woong',
+                        role: 'admin'
+                    })
+                } else {
+                    reject(new Error('no access'))
+                }
+            }, 1000)
+        })
     }
 }
 
 const userStorage = new UserStorage();
 const id = prompt('enter your id')
 const password = prompt('enter your password')
-userStorage.loginUser(
-    id,
-    password,
-    (user) => {
-        userStorage.getRoles(
-            user,
-            userWithRole => {
-                alert(`Hello ${userWithRole.name}, you have a ${userWithRole.role} role`)
-            })
-    }, (error) => {
-        console.log(error)
-    })
+userStorage
+    .loginUser(id, password)
+    .then(user => userStorage.getRoles)
+    .then(user => alert(`Hello ${user.name}, you have a ${user.role} role`))
+    .catch(console.log)
